492b606c8b604cc285a94e4c8b63827d25bb36b7
LuanComputacao <eng.luanroger@gmail.com>
2021-12-06 17:29:54 -0300

    feat: add finishing callback function

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var QuizModal = {
+let QuizModal = {
   selector: '#quiz-modal',
 
   /**
    * Initialize the modal
    *
-   * @param {Object} options
+   * @param {Object} options {selector: '#quiz-modal', loop: false}
+   * @param {Function} callback postQuizHandler(quiz)
    * @returns void
    */
-  init: function (options) {
-    this.options = {}
+  init: function (options, callback) {
+    this.options = options;
     this.selector = this.options["selector"] || this.selector;
     this.loop = this.options["loop"] || false;
+    this.callback = callback;
     this.bindEvents();
     console.log('QuizModal initialized');
   },
@@ -21,8 +23,12 @@ var QuizModal = {
   },
 
   onLoad: function () {
-
     this.storeElements();
+
+    this.initial = true;
+    this.completed = false;
+
+    this.getQuestions();
 
     this.quiz.addEventListener('click', this.onClick.bind(this));
     this.setQuizWidths()
@@ -30,6 +36,18 @@ var QuizModal = {
     this.quizQuestionsContainer.style.left = 0
 
     console.log('QuizModal loaded');
+  },
+
+  getQuestions: function () {
+    this.questions = {};
+    this.quiz.querySelectorAll('.quiz-question').forEach(function (question, index) {
+      question.setAttribute('data-index', index);
+      this.questions[index] = {
+        title: question.querySelector('.quiz-question-title').innerHTML || '',
+        answer: null
+      }
+    }.bind(this));
+    console.log(this.questions)
   },
 
   storeElements: function () {
@@ -46,6 +64,8 @@ var QuizModal = {
 
 
     if (target.classList.contains('quiz-radio')) {
+      this.storeAnswers(target);
+      this.setQuizState();
       setTimeout(function () {
         this.moveForward()
       }.bind(this), 500);
@@ -61,10 +81,17 @@ var QuizModal = {
       this.movePrevious();
     }
 
+    this.callback(this, target);
+
   },
 
   setQuizWidths: function () {
     this.quizBodyOffsetWidth = parseInt(this.quiz.querySelector('.quiz-body').offsetWidth);
+    if (this.quizBodyOffsetWidth == 0) {
+      if (this.quizBodyOffsetWidth > 0) {
+        this.quizBodyOffsetWidth = 600;
+      }
+    }
     this.quizQuestions = this.quiz.querySelectorAll('.quiz-question');
     this.quizQuestions.forEach(function (question) {
       question.style.width = this.quizBodyOffsetWidth + 'px';
@@ -116,8 +143,40 @@ var QuizModal = {
     } else {
       this.quizQuestionsContainer.style.left = newPosition + "px";
     }
+  },
+
+  storeAnswers: function (target) {
+    const name = target.getAttribute('name')
+    const value = this.quiz.querySelector('input[name="' + name + '"]:checked').value;
+    const questionIndex = target.parentElement.parentElement.getAttribute('data-index')
+
+    this.questions[questionIndex].answer = value;
+  },
+
+  setQuizState: function () {
+    var count = 0;
+    for (var key in this.questions) {
+      if (this.questions[key].answer) {
+        count++;
+      }
+    }
+
+    this.completed = count === Object.keys(QuizModal.questions).length;
   }
+
 };
 
 
-QuizModal.init({});
+function postQuizHandler(quiz, target) {
+  if (quiz.completed) {
+    console.log('end')
+    quiz.quiz.querySelector('.quiz-button-submit').style.display = 'block';
+  }
+
+  if (target.classList.contains('quiz-button-submit')) {
+    console.log(quiz.questions)
+    console.log("submit");
+  }
+}
+
+QuizModal.init({}, postQuizHandler);
